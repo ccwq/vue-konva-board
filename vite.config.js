1dc4cc3d5bc6f0df90b5d6a6440484599ba4d396
ccwq <scwqd@qq.com>
2024-12-10 17:49:14 +0800

    feat: 添加自由绘制功能

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import legacy from '@vitejs/plugin-legacy'
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [
+    vue(),
     legacy({
       targets: ['defaults', 'not IE 11']
     })
